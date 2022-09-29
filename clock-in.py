16f1085dd7fc4f2cbfd120d0bdf40f26b21c8673
hxwzljk <105581841+hxwzljk@users.noreply.github.com>
2022-09-29 20:27:52 +0800

    Update clock-in.py

--- clock-in.py
+++ clock-in.py
@@ -25,7 +25,7 @@ class ClockIn(object):
     LOGIN_URL = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
     BASE_URL = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
     SAVE_URL = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
-    CAPTCHA_URL = 'https://healthreport.zju.edu.cn/ncov/wap/default/code'
+   # CAPTCHA_URL = 'https://healthreport.zju.edu.cn/ncov/wap/default/code'
     HEADERS = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
     }
@@ -73,9 +73,9 @@ class ClockIn(object):
         """Get CAPTCHA code"""
         resp = self.sess.get(self.CAPTCHA_URL)
         captcha = self.ocr.classification(resp.content)
-        print("验证码：", captcha)
-        return captcha
-
+       # print("验证码：", captcha)
+       # return captcha
+#
     def get_info(self, html=None):
         """Get hitcard info, which is the old info with updated new time."""
         if not html:
@@ -118,7 +118,7 @@ class ClockIn(object):
         new_info['gwszdd'] = ""
         new_info['szgjcs'] = ""
         new_info['verifyCode'] = self.get_captcha()
-
+#
         # 2021.08.05 Fix 2
         magics = re.findall(r'"([0-9a-f]{32})":\s*"([^\"]+)"', html)
         for item in magics:
