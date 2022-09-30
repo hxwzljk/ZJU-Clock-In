5e43464f33f38a9f94748abb92cb4da825f2990d
hxwzljk <105581841+hxwzljk@users.noreply.github.com>
2022-09-30 10:16:07 +0800

    Update clock-in.py

--- clock-in.py
+++ clock-in.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# 鎵撳崱鑴氫慨鏀硅嚜ZJU-nCov-Hitcarder鐨勫紑婧愪唬鐮侊紝鎰熻阿杩欎綅鍚屽寮€婧愮殑浠ｇ爜
+# 打卡脚修改自ZJU-nCov-Hitcarder的开源代码，感谢这位同学开源的代码
 
 import requests
 import json
@@ -14,13 +14,13 @@ class ClockIn(object):
     """Hit card class
 
     Attributes:
-        username: (str) 娴欏ぇ缁熶竴璁よ瘉骞冲彴鐢ㄦ埛鍚嶏紙涓€鑸负瀛﹀彿锛�
-        password: (str) 娴欏ぇ缁熶竴璁よ瘉骞冲彴瀵嗙爜
-        LOGIN_URL: (str) 鐧诲綍url
-        BASE_URL: (str) 鎵撳崱棣栭〉url
-        SAVE_URL: (str) 鎻愪氦鎵撳崱url
-        HEADERS: (dir) 璇锋眰澶�
-        sess: (requests.Session) 缁熶竴鐨剆ession
+        username: (str) 浙大统一认证平台用户名（一般为学号）
+        password: (str) 浙大统一认证平台密码
+        LOGIN_URL: (str) 登录url
+        BASE_URL: (str) 打卡首页url
+        SAVE_URL: (str) 提交打卡url
+        HEADERS: (dir) 请求头
+        sess: (requests.Session) 统一的session
     """
     LOGIN_URL = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
     BASE_URL = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
@@ -53,8 +53,8 @@ class ClockIn(object):
         res = self.sess.post(url=self.LOGIN_URL, data=data, headers=self.HEADERS)
 
         # check if login successfully
-        if '缁熶竴韬唤璁よ瘉' in res.content.decode():
-            raise LoginError('鐧诲綍澶辫触锛岃鏍稿疄璐﹀彿瀵嗙爜閲嶆柊鐧诲綍')
+        if '统一身份认证' in res.content.decode():
+            raise LoginError('登录失败，请核实账号密码重新登录')
         return self.sess
 
     def post(self):
@@ -72,7 +72,7 @@ class ClockIn(object):
         if not html:
             res = self.sess.get(self.BASE_URL, headers=self.HEADERS)
             html = res.content.decode()
-            # 鏂板缓ocr锛屽苟璇诲彇楠岃瘉鐮佽繘琛岃瘑鍒�
+            # 新建ocr，并读取验证码进行识别
      #       ocr = ddddocr.DdddOcr(old=True)
      #       resp = self.sess.get(self.captcha_url, headers=self.HEADERS)
      #       captcha = ocr.classification(resp.content)
@@ -81,7 +81,7 @@ class ClockIn(object):
             if len(old_infos) != 0:
                 old_info = json.loads(old_infos[0])
             else:
-                raise RegexMatchError("鏈彂鐜扮紦瀛樹俊鎭紝璇峰厛鑷冲皯鎵嬪姩鎴愬姛鎵撳崱涓€娆″啀杩愯鑴氭湰")
+                raise RegexMatchError("未发现缓存信息，请先至少手动成功打卡一次再运行脚本")
 
             new_info_tmp = json.loads(re.findall(r'def = ({[^\n]+})', html)[0])
             new_id = new_info_tmp['id']
@@ -98,25 +98,25 @@ class ClockIn(object):
         new_info['number'] = number
         new_info["date"] = self.get_date()
         new_info["created"] = round(time.time())
-        new_info["address"] = "娴欐睙鐪佹澀宸炲競瑗挎箹鍖�"
-        new_info["area"] = "娴欐睙鐪� 鏉窞甯� 瑗挎箹鍖�"
+        new_info["address"] = "浙江省杭州市西湖区"
+        new_info["area"] = "浙江省 杭州市 西湖区"
         new_info["province"] = new_info["area"].split(' ')[0]
         new_info["city"] = new_info["area"].split(' ')[1]
         # form change
         new_info['jrdqtlqk[]'] = 0
         new_info['jrdqjcqk[]'] = 0
-        new_info['sfsqhzjkk'] = 1   # 鏄惁鐢抽鏉窞鍋ュ悍鐮�
-        new_info['sqhzjkkys'] = 1   # 鏉窞鍋ュ悍鍚楅鑹诧紝1:缁胯壊 2:绾㈣壊 3:榛勮壊
-        new_info['sfqrxxss'] = 1    # 鏄惁纭淇℃伅灞炲疄
+        new_info['sfsqhzjkk'] = 1   # 是否申领杭州健康码
+        new_info['sqhzjkkys'] = 1   # 杭州健康吗颜色，1:绿色 2:红色 3:黄色
+        new_info['sfqrxxss'] = 1    # 是否确认信息属实
         new_info['jcqzrq'] = ""
         new_info['gwszdd'] = ""
         new_info['szgjcs'] = ""
         
         # add in 2022.07.08
-        new_info['sfymqjczrj'] = 2  #鍚屼綇浜哄憳鏄惁鍙戠儹
-        new_info['ismoved'] = 4     #鏄惁鏈夌寮€
-        new_info['internship'] = 3  #鏄惁杩涜瀹炰範
-        new_info['sfcxzysx'] = 2    #鏄惁娑夊強鐤儏绠℃帶
+        new_info['sfymqjczrj'] = 2  #同住人员是否发热
+        new_info['ismoved'] = 4     #是否有离开
+        new_info['internship'] = 3  #是否进行实习
+        new_info['sfcxzysx'] = 2    #是否涉及疫情管控
         
    #     new_info['verifyCode'] = captcha
         # 2021.08.05 Fix 2
@@ -156,39 +156,39 @@ def main(username, password):
     """Hit card process
 
     Arguments:
-        username: (str) 娴欏ぇ缁熶竴璁よ瘉骞冲彴鐢ㄦ埛鍚嶏紙涓€鑸负瀛﹀彿锛�
-        password: (str) 娴欏ぇ缁熶竴璁よ瘉骞冲彴瀵嗙爜
+        username: (str) 浙大统一认证平台用户名（一般为学号）
+        password: (str) 浙大统一认证平台密码
     """
     print("\n[Time] %s" %
           datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-    print("馃殞 鎵撳崱浠诲姟鍚姩")
+    print("🚌 打卡任务启动")
 
     dk = ClockIn(username, password)
 
-    print("鐧诲綍鍒版禉澶х粺涓€韬唤璁よ瘉骞冲彴...")
+    print("登录到浙大统一身份认证平台...")
     try:
         dk.login()
-        print("宸茬櫥褰曞埌娴欏ぇ缁熶竴韬唤璁よ瘉骞冲彴")
+        print("已登录到浙大统一身份认证平台")
     except Exception as err:
         print(str(err))
         raise Exception
 
-    print('姝ｅ湪鑾峰彇涓汉淇℃伅...')
+    print('正在获取个人信息...')
     try:
         dk.get_info()
-        print('宸叉垚鍔熻幏鍙栦釜浜轰俊鎭�')
+        print('已成功获取个人信息')
     except Exception as err:
-        print('鑾峰彇淇℃伅澶辫触锛岃鎵嬪姩鎵撳崱锛屾洿澶氫俊鎭�: ' + str(err))
+        print('获取信息失败，请手动打卡，更多信息: ' + str(err))
         raise Exception
 
-    print('姝ｅ湪涓烘偍鎵撳崱')
+    print('正在为您打卡')
     try:
         res = dk.post()
         if str(res['e']) == '0':
-            print('宸蹭负鎮ㄦ墦鍗℃垚鍔燂紒')
+            print('已为您打卡成功！')
         else:
             print(res['m'])
-            if res['m'].find("宸茬粡") != -1: # 宸茬粡濉姤杩囦簡 涓嶆姤閿�
+            if res['m'].find("已经") != -1: # 已经填报过了 不报错
                 pass
             else:
                 count = 0
@@ -198,11 +198,11 @@ def main(username, password):
                     res = dk.post()
                     count +=1
                 if str(res['e']) == '0':
-                    print('宸蹭负鎮ㄦ墦鍗℃垚鍔燂紒')
+                    print('已为您打卡成功！')
                 else:
                     raise Exception
     except Exception:
-        print('鏁版嵁鎻愪氦澶辫触')
+        print('数据提交失败')
         raise Exception
 
 
